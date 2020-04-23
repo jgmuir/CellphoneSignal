d11d1c1d96689a4d3e95778c587de8c876595c9a
jgmuir <37524381+jgmuir@users.noreply.github.com>
2020-04-22 20:57:53 -0500

    Added Example Real-time Graph functionality

--- app/src/main/java/com/example/cellphonesignal/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/cellphonesignal/ui/dashboard/DashboardFragment.java
@@ -16,14 +16,22 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.cellphonesignal.R;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Random;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
 
-    LineGraphSeries<DataPoint> series;
+    private LineGraphSeries<DataPoint> series; //hold our data that is plotted
+
+    //for demonstration, will remove later
+    private static final Random RANDOM = new Random();
+    private double lastX = 0;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -33,27 +41,58 @@ public class DashboardFragment extends Fragment {
 
         double x,y; //hold the values of x and y
         x = 0.0;
-        GraphView graph = (GraphView) root.findViewById(R.id.wifiStrengthGraph);
-        graph.getViewport().setXAxisBoundsManual(true);
-        graph.getViewport().setMinX(0.0);
-        graph.getViewport().setMaxX(120);
-        graph.getViewport().setYAxisBoundsManual(true);
-        graph.getViewport().setMinY(0.0);
-        graph.getViewport().setMaxY(1.1);
-        WifiManager wifiManager = (WifiManager) getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        GraphView graph = (GraphView) root.findViewById(R.id.wifiStrengthGraph); //create graph
+        Viewport graphView = graph.getViewport();
         series = new LineGraphSeries<DataPoint>();
-        series.setTitle("Wifi Signal Strength");
-        for(int i = 0; i < 1200; i++) {
-            x += 0.1; //must go at this step to make graph look continuous
-            if(wifiManager.isWifiEnabled()) {
-                //get real time working first, then do wifiManager stuff
-                y = 1;
-            } else {
-                y = 0;
-            }
-            series.appendData(new DataPoint(x,y), true, 500);
-        }
+        //set boundaries (tinker with values)
+        graphView.setXAxisBoundsManual(true);
+        //graphView.setMinX(0.0);
+        //graphView.setMaxX(10);
+        graphView.setYAxisBoundsManual(true);
+        graphView.setMinY(0.0);
+        graphView.setMaxY(10.1);
+        graphView.setScrollable(true);
+        graph.setTitle("Wifi Signal Strength");
+        //TODO: adjust labels on horizontal axis to go by .5 increments
+
         graph.addSeries(series);
         return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //here we are going to simulate real time with thread that append data to the graph
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //we add 100 new entries
+                for(int i = 0; i < 1000; i++) {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            addEntry();
+                        }
+                    });
+
+                    //sleep to slow down thread execution (error if not in try/catch)
+                    try {
+                        Thread.sleep(500); //update by half second
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void addEntry() {
+        //for testing, will add random values
+        //in real usage, will add proper values from WifiManager
+
+        //this line says: we choose to display max of 10 points on viewport and we scroll to end
+        series.appendData(new DataPoint(lastX, RANDOM.nextDouble() * 10d), true, 12);
+        lastX += .5;
     }
 }
--- app/src/main/java/com/example/cellphonesignal/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/cellphonesignal/ui/notifications/NotificationsFragment.java
@@ -1,5 +1,7 @@
 package com.example.cellphonesignal.ui.notifications;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,23 +15,44 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.cellphonesignal.R;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
+
+    LineGraphSeries<DataPoint> series;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+        double x,y; //hold the values of x and y
+        x = 0.0;
+        GraphView graph = (GraphView) root.findViewById(R.id.wifiStrengthGraph);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0.0);
+        graph.getViewport().setMaxX(120);
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(0.0);
+        graph.getViewport().setMaxY(1.1);
+        WifiManager wifiManager = (WifiManager) getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        series = new LineGraphSeries<DataPoint>();
+        series.setTitle("Wifi Signal Strength");
+        for(int i = 0; i < 1200; i++) {
+            x += 0.1; //must go at this step to make graph look continuous
+            if(wifiManager.isWifiEnabled()) {
+                //get real time working first, then do wifiManager stuff
+                y = 1;
+            } else {
+                y = 0;
             }
-        });
+            series.appendData(new DataPoint(x,y), true, 1200);
+        }
+        graph.addSeries(series);
         return root;
     }
 }
